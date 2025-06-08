e603d0ba13151ae0ba845a32e98688846d730841
Nadir <nadirsalehsaleh@gmail.com>
2025-06-09 00:02:59 +0300

    WE HAVE A... PRODUCT!

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <SFML/Graphics.hpp>
+#include <Windows.h>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 400, 400 }), "Ralsei Pet Thingy", sf::Style::None);
+    sf::RenderWindow window(sf::VideoMode({ 400, 400 }), "Ralsei Pet Thingy");
+
+    HWND hwnd = window.getNativeHandle();
+
+    LONG exStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+    SetWindowLong(hwnd, GWL_EXSTYLE, exStyle | WS_EX_LAYERED);
+    SetLayeredWindowAttributes(hwnd, RGB(0, 0, 0), 0, LWA_COLORKEY);
+
+    //winapi magic lessgooooo
+
 
     while (window.isOpen())
     {
@@ -13,6 +23,6 @@ int main()
         }
 
         window.clear();
-        window.display(); //test
+        window.display();
     }
 }
