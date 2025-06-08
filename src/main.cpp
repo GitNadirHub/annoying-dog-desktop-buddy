c707e809b3a3404ae99f4d6aba8bda02d7cd604c
Nadir <nadirsalehsaleh@gmail.com>
2025-06-08 23:02:17 +0300

    test

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,17 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    sf::RenderWindow window(sf::VideoMode({ 400, 400 }), "Ralsei Pet Thingy", sf::Style::None);
 
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
-            {
                 window.close();
-            }
         }
 
         window.clear();
-        window.display();
+        window.display(); //test
     }
 }
